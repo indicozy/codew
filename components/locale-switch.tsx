28ceff7b0a8f68a1f6016a041f482c3e68cd2e3b
indicozy <berkutkarlibai@gmail.com>
2023-02-06 13:50:21 +0600

    god forgive me for my sins

--- components/locale-switch.tsx
+++ components/locale-switch.tsx
@@ -32,8 +32,8 @@ const Locales = ({ localeNow }: { localeNow: string }) => {
             {({ active }) => (
               <button
                 className={`${
-                  active ? "bg-violet-500 text-white" : "text-gray-900"
-                } flex uppercase  w-full items-center rounded-md px-2 py-2`}
+                  active ? "bg-default text-white" : "text-white"
+                } flex uppercase  w-full items-center rounded-xl px-2 py-2`}
                 onClick={() => updateLanguage(locale)}
               >
                 {locale}
@@ -51,12 +51,9 @@ export function LocaleSwitch() {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="uppercase inline-flex w-full justify-center rounded-full bg-default px-4 py-2 text-base font-bold text-white hover:bg-opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 font-mont">
+        <Menu.Button className="uppercase inline-flex items-center w-full justify-center rounded-full bg-default px-4 py-2 text-base font-bold text-white hover:text-rose-100 hover:bg-opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 font-mont">
           {t("locale")}
-          <IconChevronDown
-            className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
-            aria-hidden="true"
-          />
+          <IconChevronDown className="ml-2 -mr-1 h-5 w-5" aria-hidden="true" />
         </Menu.Button>
       </div>
       <Transition
@@ -68,7 +65,7 @@ export function LocaleSwitch() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 left-0 mt-2 origin-top divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 left-0 mt-2 origin-top divide-y divide-gray-700 rounded-xl bg-zinc-700 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="px-1 py-1">
             <Locales localeNow={t("locale")} />
           </div>
