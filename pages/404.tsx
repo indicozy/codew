9cec11e6d06bb659acc41b7e3cf9a6d8321b6f05
indicozy <berkutkarlibai@gmail.com>
2023-02-06 13:39:37 +0600

    god forgive me for my sins

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,21 @@
 import Container from "../components/ui/container";
+import Image from "next/image";
 
 export default function Notfound() {
   return (
-    <>
-      <Container>
-        <div className="grid grid-cols-3 h-[calc(100vh-3rem)] place-items-center text-4xl font-neue font-bold">
-          <div>oops!</div>
-          <div>404</div>
-          <div>:error</div>
-        </div>
-      </Container>
-    </>
+    <Container>
+      <Image
+        src="/assets/60.png"
+        width={2400}
+        height={2400}
+        alt="404"
+        className="fixed top-[calc(50%-24vw)] left-[calc(50%-30vw)] z-[-1] w-[60vw] sm:w-[40vw] sm:top-[calc(50%-18vw)] sm:left-[calc(50%-20vw)]"
+      />
+      <div className="grid grid-cols-3 h-[calc(100vh-4rem)] place-items-center text-4xl  sm:text-6xl font-neue font-bold">
+        <div>oops!</div>
+        <div>404</div>
+        <div>:error</div>
+      </div>
+    </Container>
   );
 }
